5efa5724ab0fc7647426c91efd1af53166b19851
jweaver <epsilonorion@gmail.com>
2017-05-26 16:23:12 -0500

    Last set of data from old system, must review

--- beginner_tut/scripts/add_two_ints_client.py
+++ beginner_tut/scripts/add_two_ints_client.py
@@ -19,11 +19,16 @@ def usage():
     return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        x = int(sys.argv[1])
-        y = int(sys.argv[2])
-    else:
-        print usage()
-        sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
-    print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
+    try:
+        rospy.init_node('add_client')
+
+        if len(sys.argv) == 3:
+            x = int(sys.argv[1])
+            y = int(sys.argv[2])
+        else:
+            print usage()
+            sys.exit(1)
+        print "Requesting %s+%s"%(x, y)
+        print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
+    except rospy.ROSInterruptException:
+        print "program interrupted before completion"
